81380dd3c1ee3fc5a9294eb0aa02dfc21d132e52
Josu31506 <josuehernandezyataco@gmail.com>
2025-04-15 17:21:41 -0500

    docs: crear el documento main.py

---
+++ src/main.py
@@ -0,0 +1,13 @@
+def main():
+    while True:
+        operacion = input("Escribe la operación (ej: 2 + 2) o 'c' para limpiar: ").strip()
+        if operacion.lower() == 'c':
+            continue
+        try:
+            resultado = eval(operacion)
+            print("Resultado:", resultado)
+        except Exception as e:
+            print("Error:", e)
+
+if __name__ == "__main__":
+    main()
