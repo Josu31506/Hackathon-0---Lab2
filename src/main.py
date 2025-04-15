b90a689fc6e97f255b9aec39b76ae816d2324dcf
FabricioLanche <fabricio.lanche@utec.edu.pe>
2025-04-15 17:45:52 -0500

    feat: Agregue la función dividir (#16)

--- src/main.py
+++ src/main.py
@@ -7,6 +7,11 @@ def restar(a, b):
 def multiplicar(a, b):
     return a * b
 
+def dividir(a, b):
+    if b == 0:
+        return "Error: División por cero"
+    return a / b
+
 def calculate():
     while True:
         operacion = input("Escribe la operación (ej: 2 + 2) o 'c' para limpiar: ").strip()
